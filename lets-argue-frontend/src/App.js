414905766f167e6111f15f6a051f95c1acb91b8a
othaderek <otha.hernandez@gmail.com>
2019-08-08 12:05:58 -0400

    Testing fetches and some iteration in app component.

--- lets-argue-frontend/src/App.js
+++ lets-argue-frontend/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    fetch('http://localhost:3000/posts')
+    .then( res => res.json())
+    .then( postObj => this.setState({posts: postObj}))
+  }
+
+  componentDidUpdate(){
+    let state = this.state.posts
+  }
+
+  state = {
+    posts: []
+  }
+
+  render () {
+
+    console.log(this.state.posts);
+    const postStuff = this.state.posts.map( post => {
+      return (
+        <div>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+          <p>{post.comments.map( comment => comment.body )}</p>
+        </div>
+      )
+    })
+
+    return(
+      <div>
+        {postStuff}
+      </div>
+    )
+
+  }
 }
 
 export default App;
